09616c5859b96e5872876ea71f9ed83d570b354b
user <bistabibek9@gmail.com>
2021-05-31 09:36:28 +0545

    projectOne

---
+++ LabOne/One.py
@@ -0,0 +1,10 @@
+#write a program that takes three numbers and print their sum
+#Every number is given on a seperate line.
+
+
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+num3=int(input("Enter Number 3:"))
+sum=num1+num2+num3
+print("three numbers are:",num1,num2,num3)
+print(f"the sum of three number is:{sum}")10
