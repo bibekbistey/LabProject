38a368dea330061cb57fb90dc23cf2aa1cb70839
user <bistabibek9@gmail.com>
2021-05-31 09:55:04 +0545

    Two

---
+++ LabOne/two.py
@@ -0,0 +1,11 @@
+
+#write a program that reads the length of the base and the height of a right-angled traingle and prints the area.
+#Every number is given on a separate line
+
+
+Length=int(input("Enter the length of a right-angled traingle:"))
+Base=int(input("enter the base of a right-angled traingle:"))
+Height=int(input("enter the height of a right-angled trainglr:"))
+Area=Length*Base*Height
+print("three sides are:",Length,Base,Height)
+print(f"The area of right-angled traingle is {Area}")
