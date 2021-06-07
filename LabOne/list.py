acce4759eacff37bb170628843429e3afbd40997
user <bistabibek9@gmail.com>
2021-06-07 09:18:36 +0545

    additionnn

---
+++ LabOne/list.py
@@ -0,0 +1,6 @@
+l1=["Bibek","Shyam","Hari","Gopal"]
+for name in l1:
+    if name.startswith("S"):
+        print("Hello {name}")
+    else:
+        print ("noooooo")
---
+++ LabOne/remainder.py
@@ -0,0 +1,3 @@
+B=int(input("enter a number"))
+for i in range(1,11):
+    print(f"{(B)} X {(i)} = {(i*B)}")
